e664dd90a37f7017db111cb0c1ae0fab4f780d29
Adam Slanina <slaninaa16@gmail.com>
2024-05-09 22:40:08 +0200

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import com.engeto.evidence.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Guest adela = new Guest("Adéla","Malíková","1993-03-13");
+        Guest jan = new Guest("Jan","Dvořáček", "1995-05-05");
+
+        Room one = new Room(1,1,true,true,1000);
+        Room two = new Room(2,1,true,true,1000);
+        Room three = new Room(3,3,false,true,2400);
+
+        Booking bookingOne = new Booking(adela,one,"2021-07-19","2021-07-26");
+        Booking bookingTwo = new Booking(adela,three,"2021-09-01","2021-09-14");
+
+        bookingTwo.addGuests(jan);
+
+        bookingList bookings = new bookingList();
+
+        bookings.addBooking(bookingOne);
+        bookings.addBooking(bookingTwo);
+
+        bookings.printBookings();
+
+
+
+
+
     }
 }
